03c620d0a3d59bd83159e4e5f32bc1d82c3de24d
rdj_68 <rsinghh68@gmail.com>
2023-04-22 13:02:00 +0530

    Alumni added

--- script/login.js
+++ script/login.js
@@ -1,33 +1,32 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import * as db from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js"
+import * as db from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDRHTl7hx5umlfi2HFnXt2kCDbNZPCw4iI",
-    authDomain: "happiness-f3909.firebaseapp.com",
-    projectId: "happiness-f3909",
-    storageBucket: "happiness-f3909.appspot.com",
-    messagingSenderId: "833703649555",
-    appId: "1:833703649555:web:c56123255b7bb49e023ae6",
-    databaseURL: "https://happiness-f3909-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyDRHTl7hx5umlfi2HFnXt2kCDbNZPCw4iI",
+  authDomain: "happiness-f3909.firebaseapp.com",
+  projectId: "happiness-f3909",
+  storageBucket: "happiness-f3909.appspot.com",
+  messagingSenderId: "833703649555",
+  appId: "1:833703649555:web:c56123255b7bb49e023ae6",
+  databaseURL: "https://happiness-f3909-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = db.getDatabase(app);
 var id = localStorage.getItem("id");
-var resultRef = db.ref(database, 'result/' + id);
+var resultRef = db.ref(database, "result/" + id);
 
-const regionsRef = db.ref(database, 'regions');
-const statesRef = db.ref(database, 'states');
+const regionsRef = db.ref(database, "regions");
+const statesRef = db.ref(database, "states");
 db.get(statesRef).then((snap) => {
+  // Remove any existing state options
+  $("#select-state > option:not([data-default])").remove();
+  const selectState = $("#select-state");
 
-    // Remove any existing state options
-    $("#select-state > option:not([data-default])").remove();
-    const selectState = $("#select-state");
-
-    snap.val().forEach((state) => {
-        selectState.append(`<option>${state}</option>`);
-    });
+  snap.val().forEach((state) => {
+    selectState.append(`<option>${state}</option>`);
+  });
 });
 
 //Append options to class
@@ -36,140 +35,143 @@ const selectClass = $("#select-class");
 selectClass.append(`<option>${"1-4"}</option>`);
 selectClass.append(`<option>${"5-8"}</option>`);
 selectClass.append(`<option>${"9-12"}</option>`);
-selectClass.append(`<option>${"staff"}</option>`);
+selectClass.append(`<option>${"Alumni"}</option>`);
 
 $(() => {
-    $("#select-state").change(() => {
-        const option = $("#select-state").find("option:selected");
-        const isDefault = option.attr("data-default") !== undefined;
+  $("#select-state").change(() => {
+    const option = $("#select-state").find("option:selected");
+    const isDefault = option.attr("data-default") !== undefined;
 
-        if (isDefault) {
-            $("#district-box").addClass("is-hidden");
-            return;
-        }
+    if (isDefault) {
+      $("#district-box").addClass("is-hidden");
+      return;
+    }
 
-        $("#help-state").addClass("is-hidden");
+    $("#help-state").addClass("is-hidden");
 
-        const state = option.val();
-        const districtRef = getDistrictRef(state);
-        loadDistricts(districtRef);
+    const state = option.val();
+    const districtRef = getDistrictRef(state);
+    loadDistricts(districtRef);
 
-        $("#district-box").removeClass("is-hidden");
-    });
+    $("#district-box").removeClass("is-hidden");
+  });
 
-    $("#select-district").change(() => {
-        $("#help-district").addClass("is-hidden");
-    });
+  $("#select-district").change(() => {
+    $("#help-district").addClass("is-hidden");
+  });
 
-    $("#input-name").change(() => {
-        $("#help-name").addClass("is-hidden");
-    });
+  $("#input-name").change(() => {
+    $("#help-name").addClass("is-hidden");
+  });
 
-    $("#input-school").change(() => {
-        $("#help-name").addClass("is-hidden");
-    });
+  $("#input-school").change(() => {
+    $("#help-name").addClass("is-hidden");
+  });
 
-    $("#input-email").change(() => {
-        $("#help-email").addClass("is-hidden");
-    });
-    $("#select-class").change(() => {
-        $("#help-class").addClass("is-hidden");
-    });
+  $("#input-email").change(() => {
+    $("#help-email").addClass("is-hidden");
+  });
+  $("#select-class").change(() => {
+    $("#help-class").addClass("is-hidden");
+  });
 
-
-    $("#login-button").on("click", onClickLogin);
+  $("#login-button").on("click", onClickLogin);
 });
 
 function getDistrictRef(state) {
-    return db.child(regionsRef, state);
+  return db.child(regionsRef, state);
 }
 
 async function loadDistricts(ref) {
-    // Remove any existing district options
-    $("#select-district > option:not([data-default])").remove();
+  // Remove any existing district options
+  $("#select-district > option:not([data-default])").remove();
 
-    const snap = await db.get(ref);
-    const selectDistrict = $("#select-district");
+  const snap = await db.get(ref);
+  const selectDistrict = $("#select-district");
 
-    snap.val().forEach((district) => {
-        selectDistrict.append(`<option>${district}</option>`);
-    });
+  snap.val().forEach((district) => {
+    selectDistrict.append(`<option>${district}</option>`);
+  });
 }
 
 function onClickLogin() {
-    const selectedState = $("#select-state").find("option:selected");
-    const selectedDistrict = $("#select-district").find("option:selected");
-    const selectedClass = $("#select-class").find("option:selected");
-    const name = $("#input-name").val();
-    const email = $("#input-email").val();
-    const school = $("#input-school").val();
+  const selectedState = $("#select-state").find("option:selected");
+  const selectedDistrict = $("#select-district").find("option:selected");
+  const selectedClass = $("#select-class").find("option:selected");
+  const name = $("#input-name").val();
+  const email = $("#input-email").val();
+  const school = $("#input-school").val();
 
-    const isSelectedStateDefault = selectedState.attr("data-default") !== undefined;
-    const isSelectedDistrictDefault = selectedDistrict.attr("data-default") !== undefined;
-    const isSelectedClassDefault = selectedDistrict.attr("data-default") !== undefined;
+  const isSelectedStateDefault =
+    selectedState.attr("data-default") !== undefined;
+  const isSelectedDistrictDefault =
+    selectedDistrict.attr("data-default") !== undefined;
+  const isSelectedClassDefault =
+    selectedDistrict.attr("data-default") !== undefined;
 
-    var hasErrors = false;
+  var hasErrors = false;
 
-    if (isSelectedStateDefault) {
-        $("#help-state").removeClass("is-hidden");
-        hasErrors = true;
-    }
-    if (isSelectedClassDefault) {
-        $("#help-class").removeClass("is-hidden");
-        hasErrors = true;
-    }
-    if (isSelectedDistrictDefault) {
-        $("#help-district").removeClass("is-hidden");
-        hasErrors = true;
-    }
-    if (typeof name === "string" && name.trim() === "") {
-        $("#help-name").removeClass("is-hidden");
-        hasErrors = true;
-    }
-    if (typeof email === "string" && email.trim() === "") {
-        $("#help-email").removeClass("is-hidden");
-        hasErrors = true;
-    }
-    if (typeof school === "string" && school.trim() === "") {
-        $("#help-school").removeClass("is-hidden");
-        hasErrors = true;
-    }
+  if (isSelectedStateDefault) {
+    $("#help-state").removeClass("is-hidden");
+    hasErrors = true;
+  }
+  if (isSelectedClassDefault) {
+    $("#help-class").removeClass("is-hidden");
+    hasErrors = true;
+  }
+  if (isSelectedDistrictDefault) {
+    $("#help-district").removeClass("is-hidden");
+    hasErrors = true;
+  }
+  if (typeof name === "string" && name.trim() === "") {
+    $("#help-name").removeClass("is-hidden");
+    hasErrors = true;
+  }
+  if (typeof email === "string" && email.trim() === "") {
+    $("#help-email").removeClass("is-hidden");
+    hasErrors = true;
+  }
+  if (typeof school === "string" && school.trim() === "") {
+    $("#help-school").removeClass("is-hidden");
+    hasErrors = true;
+  }
 
-    if (!hasErrors) {
-        finishLogin(
-            name,
-            email,
-            selectedState.val(),
-            selectedDistrict.val(),
-            selectedClass.val(),
-            school
-        );
-    }
+  if (!hasErrors) {
+    finishLogin(
+      name,
+      email,
+      selectedState.val(),
+      selectedDistrict.val(),
+      selectedClass.val(),
+      school
+    );
+  }
 }
 
 //Update result to database
 function updateResult(result, resultReference) {
-    db.set(resultReference, {
-        info: result
-    });
+  db.set(resultReference, {
+    info: result,
+  });
 }
 
-function finishLogin(name, email, state, district,classChoice,school) {
-    const obj = {
-        name: name,
-        email: email,
-        school:school,
-        state: state,
-        district: district,
-        class:classChoice
-    };
+function finishLogin(name, email, state, district, classChoice, school) {
+  const obj = {
+    name: name,
+    email: email,
+    school: school,
+    state: state,
+    district: district,
+    class: classChoice,
+  };
 
-    if (!id) {
-        resultRef = db.push(db.ref(database, 'result'));
-        id = resultRef.key
-        localStorage.setItem("id", id);
-        localStorage.setItem("class", classChoice);
-    };
-    updateResult(obj, resultRef);
-    window.location = "mentalHealth.html";
-};
+  resultRef = db.push(db.ref(database, "result"));
+  id = resultRef.key;
+  localStorage.setItem("id", id);
+  localStorage.setItem("class", classChoice);
+  console.log("hello");
+
+  console.log(localStorage.getItem("class"));
+  updateResult(obj, resultRef);
+  window.location = "mentalHealth.html";
+}
--- script/schoolSurvey.js
+++ script/schoolSurvey.js
@@ -1,9 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import { getDatabase, ref, get, update } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js"
+import {
+  getDatabase,
+  ref,
+  get,
+  update,
+} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-database.js";
 
 //to check if id exists
-const id = localStorage.getItem("id")
-checkId()
+const id = localStorage.getItem("id");
+checkId();
 // list of question IDs in the order which they must be shown
 var questionIds = null;
 
@@ -17,34 +22,34 @@ const selections = {};
 var totalScore = 0;
 var maxScore = 5;
 
-const happinessScore = localStorage.getItem("score")
+const happinessScore = localStorage.getItem("score");
 
 // Index of question ID in `questionIds` of the question currently being displayed
 var currentQuestionIndex = 0;
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDRHTl7hx5umlfi2HFnXt2kCDbNZPCw4iI",
-    authDomain: "happiness-f3909.firebaseapp.com",
-    projectId: "happiness-f3909",
-    storageBucket: "happiness-f3909.appspot.com",
-    messagingSenderId: "833703649555",
-    appId: "1:833703649555:web:c56123255b7bb49e023ae6",
-    databaseURL: "https://happiness-f3909-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyDRHTl7hx5umlfi2HFnXt2kCDbNZPCw4iI",
+  authDomain: "happiness-f3909.firebaseapp.com",
+  projectId: "happiness-f3909",
+  storageBucket: "happiness-f3909.appspot.com",
+  messagingSenderId: "833703649555",
+  appId: "1:833703649555:web:c56123255b7bb49e023ae6",
+  databaseURL: "https://happiness-f3909-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const quizRef = ref(database, 'schoolSurvey/');
+console.log(database);
+const quizRef = ref(database, "schoolSurvey/");
 
-const resultRef = ref(database, 'result/' + id)
+const resultRef = ref(database, "result/" + id);
 
 // Read questions from database
 get(quizRef).then((snap) => {
-    const questions = snap.val();
-    presentQuestions(questions);
+  const questions = snap.val();
+  presentQuestions(questions);
 });
-
 
 /**
  * Starts presenting questions received from Firebase
@@ -52,205 +57,212 @@ get(quizRef).then((snap) => {
  * @param {*} q the questions object from Firebase
  */
 function presentQuestions(q) {
-    questions = q;
-    questionIds = Object.keys(q);
-    currentQuestionIndex = 0;
+  questions = q;
+  questionIds = Object.keys(q);
+  currentQuestionIndex = 0;
 
-    displayQuestion();
+  displayQuestion();
 }
 
 function getAnswerForCurrentQuestion() {
-    return +$('input[name="answer"]:checked').val();
+  return +$('input[name="answer"]:checked').val();
 }
 
 // set handlers for buttons on window load
 window.onload = () => {
-    const quizView = $('#quiz');
+  const quizView = $("#quiz");
 
-    $('#next').on('click', function (e) {
-        e.preventDefault();
+  $("#next").on("click", function (e) {
+    e.preventDefault();
 
-        // Suspend click listener during fade animation
-        if (quizView.is(':animated')) {
-            return false;
-        }
+    // Suspend click listener during fade animation
+    if (quizView.is(":animated")) {
+      return false;
+    }
 
-        const currentQuestionId = questionIds[currentQuestionIndex];
-        selections[currentQuestionId] = getAnswerForCurrentQuestion();
+    const currentQuestionId = questionIds[currentQuestionIndex];
+    selections[currentQuestionId] = getAnswerForCurrentQuestion();
 
-        // If no user selection, progress is stopped
-        if (isNaN(selections[currentQuestionId])) {
-            alert('Please make a selection!');
-            return;
-        }
+    // If no user selection, progress is stopped
+    if (isNaN(selections[currentQuestionId])) {
+      alert("Please make a selection!");
+      return;
+    }
 
-        currentQuestionIndex = Math.min(questionIds.length - 1, currentQuestionIndex + 1);
-        displayQuestion();
-    });
+    currentQuestionIndex = Math.min(
+      questionIds.length - 1,
+      currentQuestionIndex + 1
+    );
+    displayQuestion();
+  });
 
-    $('#prev').on('click', function (e) {
-        e.preventDefault();
-        if (quizView.is(':animated')) {
-            return false;
-        }
+  $("#prev").on("click", function (e) {
+    e.preventDefault();
+    if (quizView.is(":animated")) {
+      return false;
+    }
 
-        const currentQuestionId = questionIds[currentQuestionIndex];
-        selections[currentQuestionId] = getAnswerForCurrentQuestion();
+    const currentQuestionId = questionIds[currentQuestionIndex];
+    selections[currentQuestionId] = getAnswerForCurrentQuestion();
 
-        if (isNaN(selections[currentQuestionId])) {
-            alert('Please make a selection!');
-            return;
-        }
+    if (isNaN(selections[currentQuestionId])) {
+      alert("Please make a selection!");
+      return;
+    }
 
-        currentQuestionIndex = Math.max(0, currentQuestionIndex - 1);
-        displayQuestion();
-    });
+    currentQuestionIndex = Math.max(0, currentQuestionIndex - 1);
+    displayQuestion();
+  });
 
-    $('#submit').on('click', function (e) {
-        e.preventDefault();
-        if (quizView.is(':animated')) {
-            return false;
-        }
+  $("#submit").on("click", function (e) {
+    e.preventDefault();
+    if (quizView.is(":animated")) {
+      return false;
+    }
 
-        const currentQuestionId = questionIds[currentQuestionIndex];
-        selections[currentQuestionId] = getAnswerForCurrentQuestion();
+    const currentQuestionId = questionIds[currentQuestionIndex];
+    selections[currentQuestionId] = getAnswerForCurrentQuestion();
 
+    if (isNaN(selections[currentQuestionId])) {
+      alert("Please make a selection!");
+      return;
+    }
 
-        if (isNaN(selections[currentQuestionId])) {
-            alert('Please make a selection!');
-            return;
-        }
+    //Calculation of total score
+    // var summationWeight = 0;
+    // const weightedScores = questionIds.map((questionId) => {
+    //     const invert = questions[questionId].invert;
+    //     const weight = questions[questionId].weight;
+    //     const selection = selections[questionId];
 
-        //Calculation of total score
-        // var summationWeight = 0;
-        // const weightedScores = questionIds.map((questionId) => {
-        //     const invert = questions[questionId].invert;
-        //     const weight = questions[questionId].weight;
-        //     const selection = selections[questionId];
+    //     summationWeight = summationWeight + weight;
 
-        //     summationWeight = summationWeight + weight;
+    //     return ((invert ? selection + 1 : maxScore - selection) / 5) * weight;
+    // });
+    // const weightedMean = weightedScores.reduce((a, b) => a + b) / summationWeight;
 
-        //     return ((invert ? selection + 1 : maxScore - selection) / 5) * weight;
-        // });
-        // const weightedMean = weightedScores.reduce((a, b) => a + b) / summationWeight;
-
-
-        // console.log(weightedMean);
-        displayResult(Number(happinessScore));
-        updateResult(selections, resultRef);
-        localStorage.clear("id","score")
-    });
+    // console.log(weightedMean);
+    displayResult(Number(happinessScore));
+    updateResult(selections, resultRef);
+    localStorage.clear("id", "score");
+  });
 };
 
-/** 
+/**
  * Creates a list of the answer choices as radio inputs
  */
 function createOptions() {
-    const radioList = $('<ul>');
-    const options = ["⭐","⭐⭐","⭐⭐⭐","⭐⭐⭐⭐","⭐⭐⭐⭐⭐"];
+  const radioList = $("<ul>");
+  const options = ["⭐", "⭐⭐", "⭐⭐⭐", "⭐⭐⭐⭐", "⭐⭐⭐⭐⭐"];
 
-    options.forEach((option, inx) => {
-        const item = $('<li>');
-        const input = `
+  options.forEach((option, inx) => {
+    const item = $("<li>");
+    const input = `
             <label class="radio">
                 <input type="radio" name="answer" value="${inx}"/>
                 ${option}
             </label>
-        `
-        item.append(input);
-        radioList.append(item);
-    });
+        `;
+    item.append(input);
+    radioList.append(item);
+  });
 
-    return radioList;
-}
-
-/** 
- * Creates and returns the div that contains the questions and 
- * the answer selections
- */
-function createQuestionElement(question, questionNumber) {
-    const qElement = $('<div>', {
-        id: 'question'
-    });
-
-    const header = $(`<h2 class="subtitle is-size-4">Question #${questionNumber}</h2>`);
-    qElement.append(header);
-    qElement.append($('<h3 class="is-size-5 has-text-weight-bold mb-3">').append(question.question));
-
-    const radioButtons = createOptions();
-    qElement.append(radioButtons);
-
-    return qElement;
+  return radioList;
 }
 
 /**
- * Removes any question currently being displayed from view and display 
+ * Creates and returns the div that contains the questions and
+ * the answer selections
+ */
+function createQuestionElement(question, questionNumber) {
+  const qElement = $("<div>", {
+    id: "question",
+  });
+
+  const header = $(
+    `<h2 class="subtitle is-size-4">Question #${questionNumber}</h2>`
+  );
+  qElement.append(header);
+  qElement.append(
+    $('<h3 class="is-size-5 has-text-weight-bold mb-3">').append(
+      question.question
+    )
+  );
+
+  const radioButtons = createOptions();
+  qElement.append(radioButtons);
+
+  return qElement;
+}
+
+/**
+ * Removes any question currently being displayed from view and display
  * the one required according to `currentQuestionIndex`
  */
 function displayQuestion() {
+  const quizView = $("#quiz");
+  const currentQuestionId = questionIds[currentQuestionIndex];
+  const question = questions[currentQuestionId];
 
-    const quizView = $('#quiz');
-    const currentQuestionId = questionIds[currentQuestionIndex];
-    const question = questions[currentQuestionId];
+  const progress = $("#progress");
+  progress.attr("max", questionIds.length);
+  progress.val(currentQuestionIndex);
 
-    const progress = $("#progress");
-    progress.attr("max", questionIds.length);
-    progress.val(currentQuestionIndex);
+  quizView.fadeOut(function () {
+    $("#question").remove();
 
-    quizView.fadeOut(function () {
-        $('#question').remove();
+    quizView
+      .append(createQuestionElement(question, currentQuestionIndex + 1))
+      .fadeIn();
 
-        quizView.append(createQuestionElement(question, currentQuestionIndex + 1)).fadeIn();
+    const selectedOption = selections[currentQuestionId];
 
-        const selectedOption = selections[currentQuestionId];
+    // Make the appropriate radio button checked if this question was previously answered
+    if (!isNaN(selectedOption)) {
+      $("input[value=" + selectedOption + "]").prop("checked", true);
+    }
 
-        // Make the appropriate radio button checked if this question was previously answered
-        if (!(isNaN(selectedOption))) {
-            $('input[value=' + selectedOption + ']').prop('checked', true);
-        }
-
-        // Show/hide prev and next buttons depending on
-        // whether or not we are at first or last question
-        if (currentQuestionIndex > 0) {
-            $('#prev').show();
-        } else {
-            $('#prev').hide();
-        }
-        if (currentQuestionIndex < questionIds.length - 1) {
-            $('#next').show();
-            $('#submit').hide()
-        } else {
-            $('#next').hide();
-            $('#submit').show();
-        }
-    });
+    // Show/hide prev and next buttons depending on
+    // whether or not we are at first or last question
+    if (currentQuestionIndex > 0) {
+      $("#prev").show();
+    } else {
+      $("#prev").hide();
+    }
+    if (currentQuestionIndex < questionIds.length - 1) {
+      $("#next").show();
+      $("#submit").hide();
+    } else {
+      $("#next").hide();
+      $("#submit").show();
+    }
+  });
 }
 
 //To display the final result
 function displayResult(score) {
+  $("#title").hide();
+  const quizView = $("#quiz");
 
-    $('#title').hide()
-    const quizView = $('#quiz');
+  quizView.fadeOut(function () {
+    $("#question").remove();
 
-    quizView.fadeOut(function () {
-        $('#question').remove();
-
-        quizView.append(`Your score is ${score * 100}`).fadeIn();
-        $('#prev').hide();
-        $('#submit').hide();
-    });
+    quizView.append(`Your score is ${score * 100}`).fadeIn();
+    $("#prev").hide();
+    $("#submit").hide();
+  });
 }
 
 //Update result to database
 function updateResult(result, resultReference) {
-    console.log(result)
-    update(resultReference, {
-        survey: result
-    });
+  console.log(result);
+  update(resultReference, {
+    survey: result,
+  });
 }
 //to redirect to login page if id doesnot exist
 function checkId() {
-    if (!id) {
-        window.location = "/login.html"
-    }
+  if (!id) {
+    window.location = "/login.html";
+  }
 }
